6d7f3acf340bb2ad37f357b17313a57c4df898d8
Liseth Briceño <lgbricenoa@unal.edu.co>
2017-11-11 14:56:52 -0500

    version2

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,18 @@
+class SearchController < ApplicationController
+  def create
+  	palabra = "%#{params[:keyword]}%"
+  	#sin inyección
+  	#@pets = Pet.where("name LIKE ? OR race LIKE ?",palabra,palabra)
+  	#con inyeccion
+  	@pets = Pet.where("name LIKE '#{params[:keyword]}%' OR race LIKE '#{params[:keyword]}%'")
+  	#@pets = Pet.find(:all, :conditions =>"name LIKE palabra")
+  	respond_to do |format|
+  		format.html {redirect_to root_path}
+  		format.json {render json: @pets}
+  		format.js
+  	end
+  	#render json: @pets
+  	#raise @pets.to_yaml
+  	#@pets = Pet.where("name = #{palabra} OR race = ?",palabra,palabra)
+  end
+end
